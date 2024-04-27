abc378763b71d298eb384d4bae81ee1e2ee1584e
abel goitom <abelgoitom5@gmail.com>
2024-04-26 22:09:01 -0700

    added name

---
+++ personal info.js
@@ -0,0 +1,20 @@
+let fullname ="abel";
+let lastname ="ybrah";
+let age ="27";
+let hometown ="mekelle";
+let color ="gold";
+let isstudent =true;
+let isactive =false;
+
+console.log(`fullname ${fullname}
+    lastname ${lastname}
+    age ${age}
+    hometown ${hometown}
+    color ${color}
+    isstudent ${isstudent}
+    isactive ${isactive}`);
+
+
+
+
+
